62055d34ff7602460e028a0bb3135fdf6856d263
Victor Hugo <victorferrarihugo@hotmail.com>
2024-11-23 13:18:46 -0300

    fix: Corrige alguns possíveis erros e ajusta as classes Professor e Estudante

--- GerenciamentoEstudantil/models/Estudante.java
+++ GerenciamentoEstudantil/models/Estudante.java
@@ -4,9 +4,9 @@ public class Estudante extends Pessoa {
     private String matricula;
 
     public void exibirDados() {
-        System.out.println("Nome do estudante: " + getNome());
-        System.out.println("Idade: " + getIdade());
-        System.out.println("Matrícula do estudante: " + getMatricula());
+        System.out.println("Nome do estudante: " + (getNome() !=null ? getNome() : "Sem registro"));
+        System.out.println("Idade: " + getIdade()); // Caso não tenha idade, ele não irá mostrar
+        System.out.println("Matrícula do estudante: " + (getMatricula() != null ? getMatricula() : "Sem registro"));
     }
 
     public String getMatricula() {
--- GerenciamentoEstudantil/models/Professor.java
+++ GerenciamentoEstudantil/models/Professor.java
@@ -4,9 +4,9 @@ public class Professor extends Pessoa {
     private String especialidade;
 
     public void exibirDados() {
-        System.out.println("Nome do Professor: " + getNome());
+        System.out.println("Nome do Professor: " + (getNome() !=null ? getNome() : "Sem registro"));
         System.out.println("Idade: " + getIdade());
-        System.out.println("Especialidade em: " + getEspecialidade());
+        System.out.println("Especialidade em: " + (getEspecialidade() != null ? getEspecialidade() : "Sem registro"));
     }
 
     public void setEspecialidade (String especialidade) {
