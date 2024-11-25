ded46b9f43504a7027377257f5ca44a1ac731c5d
Arthur Bendini <arthurbendini@gmail.com>
2024-11-25 17:24:44 -0300

    Abstraction of the course menu

--- src/menus/curso/MenuCurso.java
+++ src/menus/curso/MenuCurso.java
@@ -1,62 +1,27 @@
 package menus.curso;
 
-import java.awt.*;
 import javax.swing.*;
+import menus.MenuBase;
 
-public class MenuCurso {
-    public static void exibirMenu(JFrame janelaPrincipal) {
-        JFrame frame = new JFrame("Menu Curso");
-        frame.setSize(400, 300);
+public class MenuCurso extends MenuBase {
 
-        Point localizacao = janelaPrincipal.getLocation();
-        frame.setLocation(localizacao.x, localizacao.y);
+    public MenuCurso(JFrame janelaPrincipal) {
+        super("Menu Curso", janelaPrincipal);
+        construirMenu();
+    }
 
-        // Painel
-        JPanel painel = new JPanel();
-        painel.setLayout(null);
-        painel.setBackground(new Color(102, 113, 117));
-
-        // Fonte
-        Font fonte = new Font("Arial", Font.BOLD, 20);
-
+    @Override
+    protected void construirMenu() {
         JLabel label = new JLabel("Gerenciamento Estudantil");
-        JLabel label2 = new JLabel("Menu Curso");
-        JButton btnCadastrar = new JButton("Cadastrar Curso");
-        JButton btnConsultar = new JButton("Consultar Curso");
-        JButton btnSair = new JButton("Voltar");
+        JLabel subtitulo = new JLabel("Menu Professor");
+        configurarEstilo(label, subtitulo);
 
-        // Estilização
-        label.setFont(fonte);
-        label.setForeground(new Color(255,255,255));
-        label2.setForeground(new Color(255,255,255));
+        adicionarBotao(new JButton("Cadastrar Curso"), 25, 50, 200, 30, 
+            () -> { /* Implementar ação de cadastro */ });
 
-        // Ação ao apertar os botões
-        btnCadastrar.addActionListener(_ -> {
-            
-        });
+        adicionarBotao(new JButton("Consultar Curso"), 25, 100, 200, 30, 
+            () -> { /* Implementar ação de consulta */ });
 
-        btnConsultar.addActionListener(_ -> {
-            
-        });
-
-        // Posição dos botões - x, y, largura, altura
-        label.setBounds(25, 0, 300, 30);
-        label2.setBounds(25, 25, 175, 30);
-        btnCadastrar.setBounds(25, 50, 200, 30);
-        btnConsultar.setBounds(25, 100, 200, 30);
-        btnSair.setBounds(250, 225, 75, 30);
-
-        btnSair.addActionListener(_ -> frame.dispose());
-
-        // Adicionar os botões ao menu
-        painel.add(label);
-        painel.add(label2);
-        painel.add(btnCadastrar);
-        painel.add(btnConsultar);
-        painel.add(btnSair);
-
-        frame.add(painel);
-        frame.setVisible(true);
-        frame.setResizable(false);
+        adicionarBotao(new JButton("Voltar"), 250, 225, 75, 30, this::fechar);
     }
 }
